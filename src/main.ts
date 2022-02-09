5958d0a48b2e8d6c04c4f6300ab437f8263d60a0
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-02-09 17:38:36 +0200

    Setup Swagger for API-Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+  SwaggerModule
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //? Setup Swagger here
+  const config = new DocumentBuilder()
+    .setTitle('Online Auction System 📃')
+    .setDescription(
+      'API description for all endpoints of Online Auction System.'
+    )
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('/api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
