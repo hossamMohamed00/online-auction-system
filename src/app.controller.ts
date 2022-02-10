b0f3f8a0a3bce034597b76696f6478cf7c5d38f9
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-02-10 03:02:53 +0200

    Split Local auth guard in another file to avoid magic strings

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBody } from '@nestjs/swagger';
 import { Serialize } from './interceptors/serialize.interceptor';
+import { LocalAuthGuard } from './users/auth/strategies/local-auth.guard';
 import { UserDto } from './users/dto/user.dto';
 
 @Controller()
 @Serialize(UserDto)
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   @ApiBody({})
   async login(@Request() req) {
