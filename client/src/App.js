93990a9aff4472378de79b4533e8579322c083a2
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-02-25 17:26:53 +0200

    Update app.js for client

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,7 @@ function App() {
 				<img src={logo} className="App-logo" alt="logo" />
 
 				<p>
-					Edit <code>src/App.js</code> and save to reload 🔄🔄🔄🔄🔄 Edit{' '}
-					<code>src/App.js</code> and save to reload 🔄🔄🔄🔄🔄 Edit{' '}
-					<code>src/App.js</code> and save to reload 🔄🔄🔄🔄🔄 Edit{' '}
-					<code>src/App.js</code> and save to reload 🔄🔄🔄🔄🔄
+					Edit <code>src/App.js</code> and save to reload 🔄🔄🔄🔄🔄
 				</p>
 				<a
 					className="App-link"
