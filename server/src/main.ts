3f569b8019d67fb04aa5a0d0c1eb234d75c0a443
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-02-25 15:16:56 +0200

    Test Feature

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,25 +6,25 @@ import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/app.config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  //? Setup Global validation pipes
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+	//* Setup Global validation pipes
+	app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
-  //? Setup Swagger here
-  const config = new DocumentBuilder()
-    .setTitle('Online Auction System 📃')
-    .setDescription(
-      'API description for all endpoints of Online Auction System.',
-    )
-    .setVersion('1.0')
-    .build();
+	//* Setup Swagger here
+	const config = new DocumentBuilder()
+		.setTitle('Online Auction System 📃')
+		.setDescription(
+			'API description for all endpoints of Online Auction System.',
+		)
+		.setVersion('1.0')
+		.build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/api-docs', app, document);
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('/api-docs', app, document);
 
-  //? Get app config for cors settings and starting the app.
-  const appConfig: AppConfigService = app.get(AppConfigService);
-  await app.listen(appConfig.port);
+	//? Get app config for cors settings and starting the app.
+	const appConfig: AppConfigService = app.get(AppConfigService);
+	await app.listen(appConfig.port);
 }
 bootstrap();
