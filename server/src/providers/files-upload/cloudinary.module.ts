b52d5373312eb32053c24479e8e692dc06b647fd
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-05-07 14:29:15 +0200

    Add cloudinary env variables to new config module

--- server/src/providers/files-upload/cloudinary.module.ts
+++ server/src/providers/files-upload/cloudinary.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CloudinaryConfigModule } from 'src/config/cloudinary/cloudinary.config.module';
 import { CloudinaryProvider } from './cloudinary.provider';
 import { CloudinaryService } from './cloudinary.service';
 
 @Module({
+	imports: [CloudinaryConfigModule],
 	providers: [CloudinaryProvider, CloudinaryService],
 	exports: [CloudinaryProvider, CloudinaryService],
 })
--- server/src/providers/files-upload/cloudinary.provider.ts
+++ server/src/providers/files-upload/cloudinary.provider.ts
@@ -1,14 +1,18 @@
 import { v2 } from 'cloudinary';
+import { CloudinaryConfigModule } from 'src/config/cloudinary/cloudinary.config.module';
+import { CloudinaryConfigService } from 'src/config/cloudinary/cloudinary.config.service';
 import { CLOUDINARY } from './constants';
 
 export const CloudinaryProvider = {
 	provide: CLOUDINARY,
-	useFactory: () => {
-		//TODO: Create new config module to cloudinary environment variables
+	//* Inject cloudinaryConfigService to get cloudinary environment variables
+	imports: [CloudinaryConfigModule],
+	useFactory: (cloudinaryConfigService: CloudinaryConfigService) => {
 		return v2.config({
-			cloud_name: 'auctions-files',
-			api_key: '231756796911788',
-			api_secret: '95bqmL6R-QizYGnfXXe0VQ2UFIo',
+			cloud_name: cloudinaryConfigService.cloudName,
+			api_key: cloudinaryConfigService.cloudApiKey,
+			api_secret: cloudinaryConfigService.cloudApiSecret,
 		});
 	},
+	inject: [CloudinaryConfigService],
 };
