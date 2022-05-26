240d92ad85ee008aa27594e8a4343a6042b1f4a1
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-05-26 19:07:34 +0200

    Update exception message when invalid bid

--- server/src/models/bids/bid.service.ts
+++ server/src/models/bids/bid.service.ts
@@ -67,7 +67,7 @@ export class BidService {
 		if (!isValidBid) {
 			this.logger.error('Bid rejected (Invalid value) 🤷‍♂️');
 			throw new WsException(
-				'Bid value must be greater than current highest bid and opening bid as well👀',
+				'Bid value must be match with minimum bid value 👀',
 			);
 		}
 
