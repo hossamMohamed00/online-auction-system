1afea32911c41e36c30b3ca023472b11bb4c96f8
zeynab335 <zezemahmoud@domain.com>
2022-05-26 04:47:26 +0200

    change some style in add new auction

---
+++ client/src/Api/SellerApi.js
@@ -0,0 +1,23 @@
+const url = 'http://localhost:8000/seller/auction'
+
+
+
+
+export const AddNewAuctionAPI = async (AuctionDetails , idToken) => {
+
+	const response = await fetch(url, {
+			method : 'POST',
+			body : JSON.stringify({AuctionDetails}),
+			headers: {
+					Authorization: `Bearer ${idToken}`,
+					'Content-Type': 'application/json',
+			},
+	});
+	const data = await response.json()
+	if (!response.ok) {
+		throw new Error(data.message);
+
+	}
+
+	return data;
+}
--- client/src/component/Modules/SellerModule/SellerPages/AddNewAuction.js
+++ client/src/component/Modules/SellerModule/SellerPages/AddNewAuction.js
@@ -5,27 +5,54 @@ import { useNavigate } from 'react-router';
 import useHttp from '../SellerModule';
 
 import classes from './AddNewAuction.module.css';
-
 import Input from '../../../UI/Input/input';
 
 import SellerDashboardContent from '../SellerModule';
 import PageContent from '../../../UI/DashboardLayout/Pagecontant/pageContent';
 import PageHeader from '../../../UI/Page Header/pageHeader';
+import { AddNewAuctionAPI } from '../../../../Api/SellerApi';
+import { getAllCategories } from '../../../../Api/CategoryApi';
 
 const AddAuction = () => {
-	const PrudectNameRef = useRef();
-	const PrudectDescRef = useRef();
-	const TitleRef = useRef();
 
-	const [successMessage, setMessage] = useState('');
-	const [failedMessage, setFailedMessage] = useState('');
 	const idToken = useSelector(store => store.AuthData.idToken);
+	const { sendRequest, status, data, error } = useHttp(AddNewAuctionAPI);
+	const { sendRequest:sendRequestCategoryList,
+					status : statusCategoryList,
+					data : dataCategoryList,
+					error : errorCategoryList
+				} = useHttp(getAllCategories);
 
-	const validatePrudectName = value => value !== '';
 
-	const url = 'http://localhost:8000';
+	// start refs
+	const TitleRef = useRef();
+	const PrudectNameRef = useRef();
+	const BrandRef = useRef();
+	const CategoryNameRef = useRef();
+	const BasePriceRef = useRef();
+	const StartDateRef = useRef();
+	const PrudectShortDescRef = useRef();
+	const PrudectDetaildDescRef = useRef();
 
-	const { sendRequest, status, data, error } = useHttp(SellerDashboardContent);
+	// end refs
+
+	// start validation
+	const validateText = value => value !== '';
+	// const ValidateDate = '';
+
+	// end validation
+
+	// get all categories name
+	useEffect(()=>{
+		sendRequestCategoryList(idToken)
+	},[sendRequestCategoryList])
+
+	const getAllCategoriesName = statusCategoryList ==='completed' && (dataCategoryList && dataCategoryList.length > 0)
+		? dataCategoryList.map((category)=>{
+			return <option value={category.name} key={category.id}> {category.name} </option>
+			})
+		: <p> No Category Now </p>
+
 
 	const submitHandeler = e => {
 		e.preventDefault();
@@ -42,31 +69,34 @@ const AddAuction = () => {
 					<form onSubmit={submitHandeler}>
 						<div className="container">
 							<div className="row">
-								<div className={`${classes.PrdName} col-lg-6`}>
-									<label for="Title" className={'text-light fw-bold fs-5 py-2'}>
+								{/* start Product Title */}
+								<div className={`${classes.PrdName} col-lg-6 `}>
+									<label for="Title" className={'text-light fw-bold fs-6 py-2'}>
 										Titel
 									</label>
 									<Input
 										type="text"
 										placeholder=""
-										validateText={validatePrudectName}
+										validateText={validateText}
 										ref={TitleRef}
 										errorMassage="please enter Prudect Title "
 										inputValue=" Title"
 										id="Title"
 									/>
 								</div>
+
+								{/* start Product Name */}
 								<div className={`${classes.PrdName} col-lg`}>
 									<label
 										for="PrudectName"
-										className={'text-light fw-bold fs-5 py-2'}
+										className={'text-light fw-bold fs-6 py-2'}
 									>
 										product Name
 									</label>
 									<Input
 										type="text"
 										placeholder=""
-										validateText={validatePrudectName}
+										validateText={validateText}
 										ref={PrudectNameRef}
 										errorMassage="please enter Prudect Name "
 										inputValue=" prudect Name"
@@ -75,57 +105,95 @@ const AddAuction = () => {
 								</div>
 							</div>
 
-							<div className="row">
+							<div className= {` row ${classes.SelectStyl}`} >
+								{/* start Brand Name */}
 								<div className={`${classes.TextArea} col-lg-6`}>
 									<label
-										for="prudectDesc"
-										className={'text-light fw-bold fs-5 py-2 '}
+										for="Brand"
+										className={'text-light fw-bold fs-6 py-2 '}
 									>
 										Brand Name
 									</label>
 									<Input
 										type="text"
 										placeholder=""
-										validateText={validatePrudectName}
-										ref={PrudectDescRef}
+										validateText={validateText}
+										ref={BrandRef}
 										errorMassage="please enter Prudect Describtion "
 										inputValue=" prudect Describtion"
-										id="prudectDesc"
+										id="Brand"
 									/>
 								</div>
 
-								<div className={`${classes.SelectStyl} col-lg-6 `}>
-									<label className={'text-light fw-bold fs-5 py-2  '}>
-										{' '}
+								{/* start select Category Name */}
+								<div className={`col-lg-6 `}>
+									<label className={'text-light fw-bold fs-6 py-2  '}>
 										select Category
 									</label>
-									<select class="form-select " aria-label="">
-										<option> one </option>
-										<option value="1"> two</option>
+									<select class="form-select" >
+										{getAllCategoriesName}
 									</select>
 								</div>
-								<div className="row">
-									<div className={`col-lg-6`}>
+							</div>
+
+							<div className={`row ${classes.SelectStyl}`}>
+								{/* start base price */}
+								<div className="col-lg-6">
+										<label className={'text-light fw-bold fs-6 py-2 '}>
+											Base Price
+										</label>
+											<Input
+												type="number"
+												placeholder=""
+												validateText={validateText}
+												ref={BasePriceRef}
+												errorMassage="please enter Base Price "
+												inputValue=" prudect Describtion"
+												id="prudectPrice"
+											/>
+								</div>
+
+								{/* start [start date] */}
+								<div className="col-lg-6">
+										<label className={'text-light fw-bold fs-6 py-2 '}>
+											Select Start Date
+										</label>
+										<Input
+											type="date"
+											placeholder=""
+											validateText={validateText}
+											ref={StartDateRef}
+											errorMassage="please enter Start Date "
+											id="startDate"
+										/>
+								</div>
+							</div>
+
+							<div className="row">
+								{/* start short desc */}
+								<div className={`col-lg-6`}>
 										<label
 											for="prudectDesc"
-											className={'text-light fw-bold fs-5 py-2 '}
+											className={'text-light fw-bold fs-6 py-2 '}
 										>
 											product short Describtion
 										</label>
 										<Input
 											type="text"
 											placeholder=""
-											validateText={validatePrudectName}
-											ref={PrudectDescRef}
+											validateText={validateText}
+											ref={PrudectShortDescRef}
 											errorMassage="please enter Prudect Describtion "
 											inputValue=" prudect Describtion"
 											id="prudectDesc"
 										/>
-									</div>
-									<div className={` col-lg-6`}>
+								</div>
+
+								{/* start detaild desc */}
+								<div className={`col-lg-6`}>
 										<label
 											for="prudectDelitelDesc"
-											className={'text-light fw-bold fs-5 py-2 '}
+											className={'text-light fw-bold fs-6 py-2 '}
 										>
 											product Detiles Describtion
 										</label>
@@ -133,46 +201,15 @@ const AddAuction = () => {
 											placeholder="type heree..."
 											className={`form-control ${classes.ProdulctDetailed}`}
 											id="prudectDelitelDesc"
+											ref={PrudectDetaildDescRef}
 										></textarea>
-									</div>
 								</div>
+							</div>
 
-								<div className={`row ${classes.SelectStyl}`}>
-									<div className="col-lg-6">
-										<label className={'text-light fw-bold fs-5 py-2 '}>
-											Base Price
-										</label>
-										<div className={`${classes.TextArea}`}>
-											<Input
-												type="number"
-												placeholder=""
-												validateText={validatePrudectName}
-												ref={PrudectDescRef}
-												errorMassage="please enter Base Price "
-												// inputValue=" prudect Describtion"
-												id="prudectPrice"
-											/>
-										</div>
-									</div>
-									<div className="col-lg-6">
-										<label className={'text-light fw-bold fs-5 py-2 '}>
-											Select Start Date
-										</label>
-										<Input
-											type="date"
-											placeholder=""
-											validateText={validatePrudectName}
-											ref={PrudectDescRef}
-											errorMassage="please enter Start Date "
-											id="startDate"
-										/>
-									</div>
-								</div>
-
-								{/* product image */}
-								<div className="row">
-									<div className="col">
-										<label className={'text-light fw-bold fs-5 py-2 '}>
+							{/* product image */}
+							<div className="row">
+									<div className="col-6">
+										<label className={'text-light fw-bold fs-6 py-2 '}>
 											product Images
 										</label>
 										<div className={`${classes.TextArea}`}>
@@ -184,13 +221,12 @@ const AddAuction = () => {
 											/>
 										</div>
 									</div>
-								</div>
-
-								<button className={`btn btn-danger ${classes.bntstyl}`}>
-									Add Auction
-								</button>
 							</div>
-						</div>
+
+							<button className={`btn btn-danger ${classes.bntstyl}`}>
+								Add Auction
+							</button>
+					</div>
 					</form>
 				</div>
 			</PageContent>
