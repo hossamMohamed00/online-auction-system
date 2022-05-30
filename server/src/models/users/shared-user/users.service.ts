38fd8860ad4bce96818b84a0ec5a1dd074e4879d
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2022-05-30 03:57:43 +0200

    Reject blocked users to login

--- server/src/models/users/shared-user/users.service.ts
+++ server/src/models/users/shared-user/users.service.ts
@@ -1,4 +1,8 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ResponseResult } from 'src/common/types';
@@ -124,12 +128,19 @@ export class UsersService {
 	 * @param userId
 	 * @param blockReason
 	 */
-	async blockUser(userId: string, blockReason: string): Promise<ResponseResult> {
+	async blockUser(
+		userId: string,
+		blockReason: string,
+	): Promise<ResponseResult> {
 		//* Block user by update isBlock field to true and blockReason to blockReason
-		const user = await this.usersModel.findByIdAndUpdate(userId, {
-			isBlock: true,
-			blockReason,
-		}, { new: true })
+		const user = await this.usersModel.findByIdAndUpdate(
+			userId,
+			{
+				isBlocked: true,
+				blockReason,
+			},
+			{ new: true },
+		);
 
 		if (!user) {
 			throw new BadRequestException('User not found ❌');
