b0ab6e2adbf84a7c1a14df03f21e1d8bfc8a3432
zeynab335 <zezemahmoud@domain.com>
2022-06-03 05:15:54 +0200

    delete search from home-page

--- client/src/component/HomePage/Header/Navbar.js
+++ client/src/component/HomePage/Header/Navbar.js
@@ -13,16 +13,13 @@ import {
 import Categories from '../Categories/Categories';
 import classes from './Navbar.module.css';
 import DropDownBox from './DropDownBox';
-import Search from './Search';
 import { Col, Row } from 'react-bootstrap';
 
 const activeLink = navData =>
 	`${navData.isActive ? classes.active : ''} fw-bold ${classes['navLink']} `;
 
 const Navbar = props => {
-	
-	console.log(props)
-	const isLoggedIn = useSelector(store => store.AuthData.isLoggedIn);
+		const isLoggedIn = useSelector(store => store.AuthData.isLoggedIn);
 
 	const [isShownNavContent, setIsShownNavContent] = useState(false);
 	const [isShownCategoriesContent, setIsShownCategoriesContent] = useState(
@@ -68,8 +65,6 @@ const Navbar = props => {
 			</NavLink>
 		</>
 	);
-	console.log(props.showWalletHandler);
-
 	const showCategories = (
 		<div
 			className={`text-start pb-0 fw-bold ${classes.navLink} `}
@@ -110,7 +105,7 @@ const Navbar = props => {
 		>
 			<FontAwesomeIcon icon={faUser} className="px-1" /> {emailName}
 			{isShownProfileContent && (
-				<DropDownBox showWalletHandler={() => props.showWalletHandler()} />
+				<DropDownBox />
 			)}
 		</div>
 	);
@@ -119,7 +114,7 @@ const Navbar = props => {
 		<nav className={`${classes.nav} navbar navbar-dark fixed-top px-1  `}>
 			<div className="container-fluid">
 				<Row className="w-100 m-0 p-0">
-					<Col lg={2} xs={8} md={4} className={`p-1`}>
+					<Col lg={6} xs={8} md={4} className={`p-1`}>
 						<FontAwesomeIcon
 							icon={faBars}
 							className={` ${classes.faBars} text-light d-inline-block d-xs d-lg-none pt-1 px-2  `}
@@ -133,9 +128,9 @@ const Navbar = props => {
 						</Link>
 					</Col>
 
-					<Col lg={4} md={8} xs={12}>
+					{/* <Col lg={4} md={8} xs={12}>
 						<Search />
-					</Col>
+					</Col> */}
 
 					<Col className={classes.HeaderLinks} lg={6} md={12}>
 						<div
