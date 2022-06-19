4c43c3e0dad1224174bea3d9ca3d09bfdbc9627f
zeynab335 <zezemahmoud@domain.com>
2022-06-19 23:05:48 +0200

    delete notification from home page when auction-start or end

--- client/src/component/UI/CountDownTimer/CountDownTimer.js
+++ client/src/component/UI/CountDownTimer/CountDownTimer.js
@@ -1,38 +1,22 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback,  } from 'react';
 import { ToastContainer } from 'react-bootstrap';
-import { toast } from 'react-toastify';
 import useTimer from '../../../CustomHooks/useTimer';
 
 import classes from './CountDownTimer.module.css';
-import StartAuctionAudio_ from '../../../assets/Audio/start.mp3'
-import EndAuctionAudio_ from '../../../assets/Audio/finished.mp3'
 
 
 const CountDownTimer = ({AuctionDate , status}) => {
-
-	// initialize audio
-	const StartAuctionAudio = new Audio(StartAuctionAudio_)
-	const EndAuctionAudio = new Audio(EndAuctionAudio_)
-
 
 	const getDate = useCallback((AuctionDate) => {
 		const timer = useTimer(new Date(AuctionDate));
 		let timeOut , timeOut2
 		if(timer.isTimeUp && status === 'upcoming' ){
-			toast.success(`Auction Will Be UpGoing Now, If You Ready For Bidding , Join Now ❤️‍🔥`)
-			StartAuctionAudio.play()
 			timeOut = setTimeout(()=>{
-				StartAuctionAudio.pause()
 				window.location.reload()
 			},3000)
 		}
 		if(timer.isTimeUp && status === 'ongoing' ){
-			toast.success(`Auction will Be Closed Now 💔🙃`)
-			EndAuctionAudio.play()
-
 			timeOut2 = setTimeout(()=>{
-				EndAuctionAudio.pause()
-
 				window.location.reload()
 
 			},3000)
